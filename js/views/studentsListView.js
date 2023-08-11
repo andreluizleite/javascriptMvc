8ddd42b0ab6500f8bdb9779608d4dff37e099a76
@andreluizleite <andreluiz_leite@hotmail.com>
2023-08-11 20:59:36 +0100

    create/ delete/ list students

--- js/views/studentsListView.js
+++ js/views/studentsListView.js
@@ -1,9 +1,23 @@
 export class StudentsListView {
     constructor() {
-        // ... (Initialize list table) ...
+        this.tableElement = document.getElementById('studentsTable');
+        this.tbodyElement = this.tableElement.querySelector('tbody');
     }
 
-    render(students, deleteStudentCallback) {
-        // ... (Render students list) ...
+    render(students, deleteStudent) {
+        this.tbodyElement.innerHTML = '';
+
+        for (const student of students) {
+            const row = this.tbodyElement.insertRow();
+            row.insertCell(0).textContent = student.name;
+            row.insertCell(1).textContent = student.age;
+            row.insertCell(2).textContent = student.grade;
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+            deleteButton.addEventListener('click', () => {
+                deleteStudent(student.id);
+            });
+            row.insertCell(3).appendChild(deleteButton);
+        }
     }
 }
