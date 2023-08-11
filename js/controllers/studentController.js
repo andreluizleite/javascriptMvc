48fbd1f96a911027999ab11b42e343047016900c
andreluizleite <andreluiz_leite@hotmail.com>
2023-08-11 19:49:37 +0100

    files structure

---
+++ js/controllers/studentController.js
@@ -0,0 +1,31 @@
+import { StudentFormView } from '../views/studentFormView.js';
+import { StudentsListView } from '../views/studentsListView.js';
+import { StudentModel } from '../models/studentModel.js';
+
+export class StudentController {
+    constructor() {
+        this.studentFormView = new StudentFormView();
+        this.studentsListView = new StudentsListView();
+        this.studentModel = new StudentModel();
+    }
+
+    initialize() {
+        this.studentFormView.init(this.addStudent.bind(this));
+        this.updateStudentsList();
+    }
+
+    addStudent(studentData) {
+        this.studentModel.addStudent(studentData);
+        this.updateStudentsList();
+    }
+
+    updateStudentsList() {
+        const students = this.studentModel.getAllStudents();
+        this.studentsListView.render(students, this.deleteStudent.bind(this));
+    }
+
+    deleteStudent(studentId) {
+        this.studentModel.deleteStudent(studentId);
+        this.updateStudentsList();
+    }
+}
---
+++ js/models/studentModel.js
@@ -0,0 +1,17 @@
+export class StudentModel {
+    constructor() {
+        this.students = [];
+    }
+
+    addStudent(studentData) {
+        // ... (Add student logic) ...
+    }
+
+    getAllStudents() {
+        // ... (Get all students logic) ...
+    }
+
+    deleteStudent(studentId) {
+        // ... (Delete student logic) ...
+    }
+}
